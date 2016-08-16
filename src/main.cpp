16720e1432834ef13f9c7721f28864ca775c4169
Rafael Guglielmetti <rafael@rgug.ch>
2016-08-16 08:33:01 +0200

    main.cpp: c --> c++ for IO

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 #include <string>
-#include <vector>
 
 #include "coxiter.h"
 
@@ -8,27 +7,17 @@ using namespace std;
 
 int main(int argc, char *argv[]) 
 {
-	cout << "a" << endl;
+	string strInput;
+	cout << "Child Process" << endl; 
+	cout << "-------------" << endl;
 	
-	vector< vector< unsigned int > > iCoxeterMatrix( vector< vector< unsigned int > >( 3, vector< unsigned int >( 3, 2 ) ) );
-	iCoxeterMatrix[0][1] = iCoxeterMatrix[1][0] = 4;
-	iCoxeterMatrix[1][2] = iCoxeterMatrix[2][1] = 0;
-	
-	CoxIter ci( iCoxeterMatrix, 2 );
-	cout << "Run: " << ci.bRunAllComputations() << endl;
-	cout << implode( ", ", ci.get_iFVector() ) << endl;
-	unsigned int iDimension(4);
-	MPZ_rational cov( ( iDimension / 2 ) % 2 ? -1 : 1 );
-	for( unsigned int i( 1 ); i <= iDimension; i++ )
-	{
-		cov *= 2;
-		cov /= i;
-		if( i <= ( iDimension / 2 ) )
-			cov *= i;
+	do{ 
+		getline( std::cin, strInput );
+		
+		if( strInput.find( "exit" ) != string::npos )
+			break;
 	}
-	
-	cout << "Euler char: " << cov * ci.get_brEulerCaracteristic( ) << endl;
-	cout << "pi^" << ( iDimension / 2 ) << " * " << cov * ci.get_brEulerCaracteristic( ) << endl;
+	while( true );
 	
 	return 0;
 }
