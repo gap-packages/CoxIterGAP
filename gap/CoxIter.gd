441468f7fbbc443ebe7e58f8017f20fc0e21a2cc
Rafael Guglielmetti <rafael@rgug.ch>
2016-10-16 14:32:39 +0200

    Documentation of the operation for AutoDoc

--- gap/CoxIter.gd
+++ gap/CoxIter.gd
@@ -18,8 +18,21 @@ IsCoxIterRep := NewRepresentation("IsCoxIter", IsObject and IsComponentObjectRep
 
 CoxIterType := NewType( CoxIterFamily, IsCoxIter and IsCoxIterRep and IsAttributeStoringRep );
 
+#! @Arguments gr, dimension
+#! @Returns a CoxIter object
+#! @Description
+#!  Creates a CoxIter object from the Coxeter graph <A>gr</A>. If the dimension <A>dim</A> is unknown, 0 can be given.
 DeclareOperation( "CreateCoxIterFromCoxeterGraph", [ IsList, IsInt ] );
+
+#! @Arguments mat, dimension
+#! @Returns a CoxIter object
+#! @Description
+#!  Creates a CoxIter object from the Coxeter matrix <A>mat</A>. If the dimension <A>dim</A> is unknown, 0 can be given.
 DeclareOperation( "CreateCoxIterFromCoxeterMatrix", [ IsMatrix, IsInt ] );
+
+#! @Arguments ci
+#! @Description
+#!  Compute the invariants of the Coxiter object <A>ci</A>
 DeclareOperation( "CoxIterCompute", [ IsCoxIter ] );
 
 DeclareGlobalVariable( "_CoxIter_MAXSIZES", "Maximum: weight for an edge, index for a vertex, dimension" );
